31538ffe754880953da754b3751c5c7b5e667e7d
gal <anlaganlag>
2020-07-12 13:27:24 +0800

    绑定已勾选

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,17 +4,25 @@ import PropTypes from 'prop-types';
 export class Todoitem extends Component {
     getStyle = () => {
         return {
+            background:"#f4f4f4",
+            padding:"10px",
+            borderBottom:"2px #ccc dotted",
             textDecoration:this.props.todo.completed ?
             'line-through':'none'
         }
     }
     
+    markComplete = e =>
+        console.log(this.props)
+    
     render() {
         return (
             <div style={ this.getStyle() }>
-                <p>{ this.props.todo.id }</p>
-                <p>{ this.props.todo.completed }</p>
-                <p>{ this.props.todo.title }</p>
+                <p>
+                    <input type="checkbox" onChange={this.markComplete} /> 
+                    {" "}
+                    { this.props.todo.title }
+                    </p>
                 
             </div>
         )
@@ -24,9 +32,6 @@ Todoitem.propTypes = {
     todo:PropTypes.object.isRequired
 }
 
-const itemStyle ={
-    backgroundColor:"#bfa"
-}
 export default Todoitem
 
 
