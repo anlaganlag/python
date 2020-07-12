7dc4791954048ca4ef97db696519662cd4d82b96
gal <anlaganlag>
2020-07-12 15:43:46 +0800

    提交内容

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react'
 
 export class AddTodo extends Component {
+    state = {
+        title:''
+    }
+    onSubmit = (e) => { 
+        e.preventDefault();
+        this.props.addTodo(this.state.title);
+        this.setState({ title: '' });
+    }
+    onChange = (e) => this.setState({ [e.target.name]:e.target.value} );
     render() {
         return (
-            <form style={{ display:"flex",padding:"5px" }}>
+            <form onSubmit={this.onSubmit} style={{ display:"flex",padding:"5px" }}>
                 <input 
                     type="text" 
                     name="title" 
                     style={{ flex:"10" }}
                     placeholder="Add Todo..." 
+                    value={this.state.title}
+                    onChange={this.onChange}
                 />
          
             <input 
