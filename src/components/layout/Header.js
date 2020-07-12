2caf64184ecd311440319df57a9c145fd5fd11ce
gal <anlaganlag>
2020-07-12 16:20:43 +0800

    基本完成可以考虑用api

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
     return (
         <header style={headerStyle}>
           <h1>TodoListk</h1>  
+          <Link style={linkStyle} to="/">Home</Link> | <Link style={linkStyle} to="/about">About</Link>
         </header>
     )
 }
@@ -12,5 +14,10 @@ const headerStyle = {
     color:'#fff',
     textAlign:'center',
     padding:'30px'
+}
+
+const linkStyle = {
+    color:'#fff',
+    textDecoration:'none'
 }
 export default Header;
