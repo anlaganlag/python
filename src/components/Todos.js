2fc5c7aeb70a3e5640e3afd918ddc0e22f0059d7
gal <anlaganlag>
2020-07-12 10:52:38 +0800

    加入propTyeps

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import TodoItem from "./TodoItem";
+import PropTypes from 'prop-types';
+
 
 class Todos extends Component {
     render() {
@@ -8,5 +10,7 @@ class Todos extends Component {
         ));
     }
 }
-
+Todos.propTypes = {
+    todo:PropTypes.array.isRequired
+}
 export default Todos;
